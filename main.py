2712594afd229699dbf6465c00b187890fa928dd
Niels <nielsb1@hotmail.com>
2016-04-08 15:27:30 +0200

    No longer store image buffers in memory, write them asap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 import getopt
 import logging
-import timeit
 import os
 from dateutil.parser import parse
 from exifread.tags import DEFAULT_STOP_TAG, FIELD_TYPES
@@ -58,61 +57,38 @@ def get_sorting_option(question,res,prev_ans= None,prev_res = None):
 def execute(folder, destination, sort_option, mode):
     
     if os.path.isdir(folder):
-        dates = {}
-        count = 0
         count_files = 0
     
         if mode == 'r':
             for folder, subs, files in os.walk(folder):
                 for file in files:
                     if os.path.splitext(file)[1] == '.tif' or os.path.splitext(file)[1] == '.tiff'or os.path.splitext(file)[1] == '.jpg' or os.path.splitext(file)[1] == '.jpeg':
-                        walk_images(file, dates, folder, destination, sort_option, exif_options)
+                        walk_images(file, folder, destination, sort_option, exif_options)
                         count_files += 1
             print('Found image files ' + str(count_files))
         else:
             files = [f for f in os.listdir(folder) if os.path.splitext(f)[1] == '.tif' or os.path.splitext(f)[1] == '.tiff' or os.path.splitext(f)[1] == '.jpg' or os.path.splitext(f)[1] == '.jpeg']
-            print('Found image files ' + str(len(files)))
             for file in files:
-                walk_images(file, dates, folder, destination, sort_option, exif_options)
+                walk_images(file, folder, destination, sort_option, exif_options)
+                count_files += 1
         
-
-        for date in dates:
-            make_sure_path_exists("./"+date)
-            
-            for filename,file in dates.get(date).items():
-                count +=1
-                if len(destination) > 0:
-                    write_file(filename,destination+'/'+date+'/',file)
-                else:
-                    write_file(filename,date+'/',file)
-        print('Processed image files ' + str(count))   
+        print('Processed image files ' + str(count_files))   
     else:
         print('Invalid folder given')
 
-def walk_images(filename, dates, folder, destination, sort_option, exif_options):
+def walk_images(filename, folder, destination, sort_option, exif_options):
     
-    file_start = timeit.default_timer()
-    
-    try:
-        img_file = open(str(folder+'/'+filename), 'rb')
-        img_orig_writeback = open(str(folder+'/'+filename), 'rb') # i dont like it but easiest fix
-    except IOError:
-        logger.error("'%s' is unreadable", filename)
-    
-    logger.info("Opening: %s", filename)
-
-    tag_start = timeit.default_timer()
+    img_file = open(str(folder+'/'+filename), 'rb')
+    img_orig_writeback = open(str(folder+'/'+filename), 'rb') # i dont like it but easiest fix
     
     img_file_orig = img_orig_writeback.read()
-    file_name = str(img_file_orig)[29:][:-2]
+    img_orig_writeback.close()
 
     try:
         # this process removes bytes, second buffer is a fix for this
         data = process_file(img_file, stop_tag=exif_options.stop_tag, details=exif_options.detailed, strict=exif_options.strict, debug=exif_options.debug)
     except UnicodeDecodeError:
-        pass
-
-    tag_stop = timeit.default_timer()
+        print("unicode error")
 
     if not data:
         logger.info("No EXIF information found\n")
@@ -140,19 +116,11 @@ def walk_images(filename, dates, folder, destination, sort_option, exif_options)
         elif sort_option == 2: # year
            date = date[:-6]
             
-        if dates.get(date) == None:
-            dates[date] = {filename:img_file_orig}
+        if len(destination) > 0:
+            write_file(filename,destination+'/'+date+'/',img_file_orig)
         else:
-            # add the data to the existing key
-            dates.get(date)[filename]=img_file_orig
-        
-        logger.info(data['Image DateTime'].printable)
+            write_file(filename,date+'/',img_file_orig)
 
-    file_stop = timeit.default_timer()
-
-    logger.debug("Tags processed in %s seconds", tag_stop - tag_start)
-    logger.debug("File processed in %s seconds", file_stop - file_start)
-                    
 
 def make_sure_path_exists(path):
     try:
